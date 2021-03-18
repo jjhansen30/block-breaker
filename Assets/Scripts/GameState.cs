2205b376bf51c3d0f5b00d02d1bfb366d6aaa26b
jjhansen30 <35323096+jjhansen30@users.noreply.github.com>
2021-03-18 08:01:49 -0600

    add game state to scenes and adjust game speed range

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    [Range(0, 2f)] [SerializeField] float gameSpeed;
+    [Range(0, 10f)] [SerializeField] float gameSpeed;
 
     // Start is called before the first frame update
     void Start()
