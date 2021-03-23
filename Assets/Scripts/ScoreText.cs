daf21f089dd8b44e8fe6c6f79c644b065978bdb8
jjhansen30 <35323096+jjhansen30@users.noreply.github.com>
2021-03-23 09:00:59 -0600

    Score to presist across scenes

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    private void Awake()
+    {
+        int textCount = FindObjectsOfType<Canvas>().Length;
+        Debug.Log("textCount is " + textCount);
+        if (textCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Log("Logging Start()");
+        Debug.Log("number of canvas'" + FindObjectsOfType<Canvas>().Length);
+    }
+}
