69ef5ac3e30b2748eac82f0232336c647540eadb
jjhansen30 <35323096+jjhansen30@users.noreply.github.com>
2021-03-23 08:30:02 -0600

    Adding Score

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,6 +7,7 @@ public class Block : MonoBehaviour
     // Config param
     [SerializeField] int amountToBreak = 3;
     [SerializeField] AudioClip breakSound;
+    [SerializeField] int pointValue = 10;
 
     //Cached component reference
     int countCollisions = 0;
@@ -21,9 +22,15 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
         countCollisions++;
         if (countCollisions == amountToBreak)
         {
+            FindObjectOfType<GameState>().IncreaseScore(pointValue);
             level.CountDestroyedBlocks();
             AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
             Destroy(gameObject);
